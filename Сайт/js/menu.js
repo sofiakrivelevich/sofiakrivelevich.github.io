45925526d425f2b88b2bca6742039f3400043742
sofiakrivelevich <60138595+sofiakrivelevich@users.noreply.github.com>
2020-05-26 17:35:27 +0300

    Delete menu.js

--- Сайт/js/menu.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector('#menu-toggle');
-const MobileNavContainer=document.querySelector('#mobile_nav');
-
-menuToggle.onclick = function(){
-  menuToggle.classList.toggle('menu-icon-active');
-  MobileNavContainer.classList.toggle('mobile_nav_active');
-}
