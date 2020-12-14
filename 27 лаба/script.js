6540b31bd81f16d13ee127c4b9f6911aefeada14
sofiakrivelevich <60138595+sofiakrivelevich@users.noreply.github.com>
2020-12-14 21:19:13 +0300

    /

--- 27 лаба/script.js
+++ 27 лаба/script.js
@@ -1,83 +1,83 @@
 document.getElementsByClassName("joke_img")[0].onmouseover = function () {
     var a = document.getElementsByClassName("joke_img")[0];
-    a.src = "1.gif"
+    a.src = "1.gif";
 }
 
 document.getElementsByClassName("joke_img")[0].onmouseout = function () {
     var a = document.getElementsByClassName("joke_img")[0];
-    a.src = "1.jpg"
+    a.src = "1.jpg";
 }
 
 document.getElementsByClassName("joke_img")[1].onmouseover = function () {
     var a = document.getElementsByClassName("joke_img")[1];
-    a.src = "2.gif"
+    a.src = "2.gif";
 }
 
 document.getElementsByClassName("joke_img")[1].onmouseout = function () {
     var a = document.getElementsByClassName("joke_img")[1];
-    a.src = "2.jpg"
+    a.src = "2.jpg";
 }
 
 document.getElementsByClassName("joke_img")[2].onmouseover = function () {
     var a = document.getElementsByClassName("joke_img")[2];
-    a.src = "3.gif"
+    a.src = "3.gif";
 }
 
 document.getElementsByClassName("joke_img")[2].onmouseout = function () {
     var a = document.getElementsByClassName("joke_img")[2];
-    a.src = "3.jpg"
+    a.src = "3.jpg";
 }
 
 
 document.getElementsByClassName("joke_img")[3].onmouseover = function () {
     var a = document.getElementsByClassName("joke_img")[3];
-    a.src = "4.gif"
+    a.src = "4.gif";
 }
 
 document.getElementsByClassName("joke_img")[3].onmouseout = function () {
     var a = document.getElementsByClassName("joke_img")[3];
-    a.src = "4.jpg"
+    a.src = "4.jpg";
 }
 
 document.getElementsByClassName("joke_img")[4].onmouseover = function () {
     var a = document.getElementsByClassName("joke_img")[4];
-    a.src = "5.gif"
+    a.src = "5.gif";
 }
 
 document.getElementsByClassName("joke_img")[4].onmouseout = function () {
     var a = document.getElementsByClassName("joke_img")[4];
-    a.src = "5.jpg"
+    a.src = "5.jpg";
 }
 
 document.getElementsByClassName("joke_img")[5].onmouseover = function () {
     var a = document.getElementsByClassName("joke_img")[5];
-    a.src = "6.gif"
+    a.src = "6.gif";
 }
 
 document.getElementsByClassName("joke_img")[5].onmouseout = function () {
     var a = document.getElementsByClassName("joke_img")[5];
-    a.src = "6.jpg"
+    a.src = "6.jpg";
 }
 
 document.getElementsByClassName("joke_img")[6].onmouseover = function () {
     var a = document.getElementsByClassName("joke_img")[6];
-    a.src = "7.gif"
+    a.src = "7.gif";
 }
 
 document.getElementsByClassName("joke_img")[6].onmouseout = function () {
     var a = document.getElementsByClassName("joke_img")[6];
-    a.src = "7.jpg"
+    a.src = "7.jpg";
 }
 
 
 document.getElementsByClassName("joke_img")[7].onmouseover = function () {
     var a = document.getElementsByClassName("joke_img")[7];
-    a.src = "8.gif"
+    a.src = "8.gif";
 }
 
 document.getElementsByClassName("joke_img")[7].onmouseout = function () {
     var a = document.getElementsByClassName("joke_img")[7];
-    a.src = "8.png"
+    a.src = "8.png";
 }
 
 
